66ebee5f10dd3f31d0408505ab3b89546f0de098
Framebuffer <26495556+Framebuffers@users.noreply.github.com.>
2025-11-03 21:49:57 -0300

    created test image

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from badge!")
+import sys
+import os
+import logging
+from waveshare_epd import epd2in13b_V4
+from PIL import Image,ImageDraw,ImageFont
+import traceback
 
+filename = 'doggo.bmp'
+logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == "__main__":
-    main()
+try:
+    logging.info('loading doggo picture')
+    epd.init()
+    epd.Clear()
+
+    doggoimage = Image.open(filename)
+    epd.display(epd.getbuffer(doggoimage))
+except IOError as e:
+    logging.info(e)
+except KeyboardInterrupt:
+    logging.info('interrupting...')
+    epd2in13b_V4.epdconfig.module_exit(cleanup=True)
+    exit()
